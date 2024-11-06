e82dd8253fd21c601d8389bd91a625160cda5406
Yogendra Singh <yogendrasingh1611035@gmail.com>
2024-11-06 20:40:12 +0530

    Commited by Yogendra Singh

--- src/main/java/com/yogi/main/controllers/CourseController.java
+++ src/main/java/com/yogi/main/controllers/CourseController.java
@@ -38,6 +38,7 @@ public class CourseController
 		System.out.println("---THis is my First Commit");
 		System.out.println("--How i can DO work----");
 		System.out.println();
+		System.out.println();
 		model.addAttribute("course",c );
 		model.addAttribute("successMsg", successMsg);
 		model.addAttribute("errorMsg", errorMsg);
